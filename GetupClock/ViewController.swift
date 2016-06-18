74fc43e7dca602931b712902a0fe90c52877537b
n0b15k3 <meetme.nobi@gmail.com>
2016-06-18 16:28:00 +0900

    Initial Commit

---
+++ GetupClock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetupClock
+//
+//  Created by KAZ on H28/06/18.
+//  Copyright © 平成28年 KAZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
